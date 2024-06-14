a03a7e7bbd518824999d7d2e8de4e7fafefde447
sdrcr74 <sophie.dorcier@gmail.com>
2024-06-14 09:33:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ st.sidebar.title("Sommaire")
 pages=["Le projet & jeu de données","Analyse & Datavisualisation","Préparation des données","Modélisation","Conclusion"]
 page=st.sidebar.radio("Aller à la page:", pages)
 st.sidebar.title("Auteurs")
-st.sidebar["Maxence"]
+with st.sidebar:
+    with st.echo():
+        st.write("Maxence")
 if page==pages[0]:
   st.header("Description du projet", divider='rainbow')
   st.subheader("L'objectif :")
