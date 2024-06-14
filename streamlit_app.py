4a1d8286cbef136547792667a6e786d774a5bed0
sdrcr74 <sophie.dorcier@gmail.com>
2024-06-14 13:49:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ elif page==pages[2]:
     st.plotly_chart(fig4, key="bank", on_select="rerun")
   if Graphique_sélectionné =='Répartition du nombre de contact de la dernière campagne': 
     fig4=px.box(bank,y='previous')
-    st.plotly_chart(fig4, key="bank", on_select="rerun")
+    st.plotly_chart(fig4, key="bank", on_select="rerun", width=800, height=400)
   
 elif page==pages[3]:
   st.write("DataViz")
